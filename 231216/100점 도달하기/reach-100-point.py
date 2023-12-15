7726b55fe16c4a39cd77432808dcb4fcee5f49ea
Dae-Hee-Ryu <57823242+eogml0716@users.noreply.github.com>
2023-12-16 05:13:28 +0900

    Title: 100점 도달하기, Time: 155ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231216/100점 도달하기/reach-100-point.py
@@ -0,0 +1,12 @@
+score = int(input())
+for score in range(score, 101):
+    if (score >= 90):
+        print("A", end = " ")
+    elif (score >= 80):
+        print("B", end = " ")
+    elif (score >= 70):
+        print("C", end = " ")
+    elif (score >= 60):
+        print("D", end = " ")
+    else:
+        print("F", end = " ")
