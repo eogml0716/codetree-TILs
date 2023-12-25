0528789912e41948983b798fb43c25b42d491a86
Dae-Hee-Ryu <57823242+eogml0716@users.noreply.github.com>
2023-12-26 00:17:07 +0900

    Title: 조건에 따른 연산, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231226/조건에 따른 연산/operatino-by-rule.py
@@ -0,0 +1,15 @@
+cnt = 0
+n = int(input())
+
+while True:
+    if n % 2 == 0:
+        n = 3 * n + 1
+    else:
+        n = 2 * n + 2
+    
+    cnt += 1
+    
+    if (n > 1000):
+        break
+    
+print(cnt)
