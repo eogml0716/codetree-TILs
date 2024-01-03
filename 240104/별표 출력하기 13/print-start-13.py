63bf1f5bd9ade1c9aa61b10cad3c16020a3aad75
Dae-Hee-Ryu <57823242+eogml0716@users.noreply.github.com>
2024-01-04 05:38:36 +0900

    Title: 별표 출력하기 13, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 240104/별표 출력하기 13/print-start-13.py
+++ 240104/별표 출력하기 13/print-start-13.py
@@ -1,19 +1,41 @@
+# n = int(input())
+
+# for i in range(n):
+#     if i % 2 == 0:
+#         for j in range(n-int(i/2)):
+#             print("*", end=" ")
+#     else:
+#         for j in range(int(i+1/2)):
+#             print("*", end=" ")
+#     print()
+
+# for i in range(n-1, -1, -1):
+#     if i % 2 == 0:
+#         for j in range(n-int(i/2)):
+#             print("*", end=" ")
+#     else:
+#         for j in range(int(i+1/2)):
+#             print("*", end=" ")
+#     print()
+
 n = int(input())
 
+# 윗쪽 별표 출력
 for i in range(n):
     if i % 2 == 0:
-        for j in range(n-int(i/2)):
+        for j in range(n - int(i / 2)):
             print("*", end=" ")
     else:
-        for j in range(int(i+1/2)):
+        for j in range(int((i + 1) / 2)):
             print("*", end=" ")
     print()
 
-for i in range(n-1, -1, -1):
+# 아랫쪽 별표 출력
+for i in range(n - 1, -1, -1):
     if i % 2 == 0:
-        for j in range(n-int(i/2)):
+        for j in range(n - int(i / 2)):
             print("*", end=" ")
     else:
-        for j in range(int(i+1/2)):
+        for j in range(int((i + 1) / 2)):
             print("*", end=" ")
     print()
