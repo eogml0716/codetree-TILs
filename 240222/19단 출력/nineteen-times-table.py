b1c3383729cb3a2ad8ea36f0e7936835b36aac61
Dae-Hee-Ryu <57823242+eogml0716@users.noreply.github.com>
2024-02-22 23:46:02 +0900

    Title: 19단 출력, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240222/19단 출력/nineteen-times-table.py
@@ -0,0 +1,47 @@
+# 일단 잘 모르겠으면 노가다를 해본 다음에 그 아래 노가다들의 규칙을 찾는 방법도 하나의 방법이겠네
+for i in range(1, 20):
+    for j in range(1, 20):
+        if i % 2 != 0:
+            if (i * j) % 2 == 0:
+                print(f"{i} * {j} = {i * j}", end = "\n")
+            elif j < 19:
+                print(f"{i} * {j} = {i * j}", end = " / ")
+            else:
+                print(f"{i} * {j} = {i * j}")
+        else:
+            if (i * j) % (i * 2) == 0:
+                print(f"{i} * {j} = {i * j}", end = "\n")
+            elif j < 19:
+                print(f"{i} * {j} = {i * j}", end = " / ")
+            else:
+                print(f"{i} * {j} = {i * j}")
+
+
+# 일단 잘 모르겠으면 노가다를 해본 다음에 그 아래 노가다들의 규칙을 찾는 방법도 하나의 방법이겠네
+
+# cnt = 1
+# for j in range(1, 20):
+#     if (cnt * j) % 2 == 0:
+#         print(f"{cnt} * {j} = {cnt * j}", end = "\n")
+#     elif j < 19:
+#         print(f"{cnt} * {j} = {cnt * j}", end = " / ")
+#     else:
+#           print(f"{cnt} * {j} = {cnt * j}")
+
+# cnt = 2
+# for j in range(1, 20):
+#     if (cnt * j) % 4 == 0:
+#         print(f"{cnt} * {j} = {cnt * j}", end = "\n")
+#     elif j < 19:
+#         print(f"{cnt} * {j} = {cnt * j}", end = " / ")
+#     else:
+#           print(f"{cnt} * {j} = {cnt * j}")
+
+# cnt = 3
+# for j in range(1, 20):
+#     if (cnt * j) % 2 == 0:
+#         print(f"{cnt} * {j} = {cnt * j}", end = "\n")
+#     elif j < 19:
+#         print(f"{cnt} * {j} = {cnt * j}", end = " / ")
+#     else:
+#           print(f"{cnt} * {j} = {cnt * j}")
