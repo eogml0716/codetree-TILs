f47d77aedac04c76ae679dfe50789e937b185319
Dae-Hee-Ryu <57823242+eogml0716@users.noreply.github.com>
2024-02-26 22:31:22 +0900

    Title: 완전수, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240226/완전수/perfect-number.py
@@ -0,0 +1,17 @@
+a, b = map(int, input().split())
+
+sum_val = 0
+temp = 0
+count_val = 0
+# 약수를 일단 어떻게 구하지?
+for i in range(a, b+1):
+    temp = i
+    while (i != 0):
+        if (temp % i == 0 and temp != i):
+            sum_val += i
+        i -= 1
+    
+    if (sum_val == temp):
+        count_val += 1
+
+print(count_val)
