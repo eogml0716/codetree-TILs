73aea2c20d8cba333590d357e228ef5913da62c4
Dae-Hee-Ryu <57823242+eogml0716@users.noreply.github.com>
2024-02-26 23:30:19 +0900

    Title: 배열 채우기, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240226/배열 채우기/filling-array.py
+++ 240226/배열 채우기/filling-array.py
@@ -1,10 +1,12 @@
 arr = list(map(int, input().split()))
 
+isTrue = False
 for i in range(len(arr)):
     if(arr[i] == 0):
+        isTrue = True
         for j in range(len(arr) - 2, -1, -1):
             print(arr[j], end = " ")
     
-if (len(arr) == 10):
+if (len(arr) == 10 and isTrue == False):
     for j in range(len(arr) - 1, -1, -1):
             print(arr[j], end = " ")
