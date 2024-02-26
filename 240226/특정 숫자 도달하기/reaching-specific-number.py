d905f6507ef8590060797a139a84cd2e22832fb4
Dae-Hee-Ryu <57823242+eogml0716@users.noreply.github.com>
2024-02-26 23:10:00 +0900

    Title: 특정 숫자 도달하기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240226/특정 숫자 도달하기/reaching-specific-number.py
+++ 240226/특정 숫자 도달하기/reaching-specific-number.py
@@ -6,7 +6,7 @@ count_val = 0
 for i in range(len(arr)):
     if (arr[i] >= 250):
         average_val = sum_val / count_val
-        print(f"{sum_val} {average_val}")
+        print(f"{sum_val} {average_val:.1f}")  # 소수점 첫째 자리까지 출력
         break
     else:
         sum_val += arr[i]
